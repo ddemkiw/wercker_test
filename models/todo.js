4ba4ba47b7f0bd1fc9a11dd99e829e8f8385c2bb
Danielle Demkiw <ddemkiw@gmail.com>
2016-01-12 15:54:56 +0000

    fixing syntax error in todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
-vwr Schema = mongoose.Schema;
-var OjectId = Schema.ObjectId;
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
 
-var todoSchema = new Schema({
-    action: String, 
-    author: String, 
-    creationDate: {type: Date, 'default':Date.now},
+var todoSchema= new Schema({
+  action: String,
+  author: String,
+  creationDate: {type: Date, 'default': Date.now},
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
