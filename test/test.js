5e1ccda3385b3bf692de6b0fd6e84dc6b2ae9341
Danielle Demkiw <ddemkiw@gmail.com>
2016-01-12 15:38:05 +0000

    fixing error in test.js file

--- test/test.js
+++ test/test.js
@@ -1,46 +1,35 @@
- var request = require('supertest')
-    , express = require('express');
+var request = require('supertest'),
+    express = require('express');
 
-  var app = require('../app.js');
+var app = require('../app.js');
 
-  describe('GET', function(){
-    it('respond with json', function(done){
-      request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-    })
-  })
-  
-  describe('POST', function(){
-    it('responds with json success message', function(done){
-        request(app)
-        .post('/todos')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .send({ 'action': 'write post of TDD with stuff', 'author':'ddemkiw'})
-        .expect(200, done);
-        });
-    });
-
-describe('GET' function(){
-    it('responds with a list of todo items in JSON',function(done){
-        request(app)
-        .get('/todos')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
+describe('POST', function(){
+  it('responds with a json success message', function(done){
+    request(app)
+    .post('/todos')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .send({'action': 'write post on TDD with mongodb, nodejs and wercker', 'author': 'mies'})
+    .expect(200, done);
+  });
 });
 
 describe('GET', function(){
-    it('responds with a single todo item in JSON based on the author', function(){
-        request(app)
-        .get('/todos/ddemkiw')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
+  it('responds with a list of todo items in JSON', function(done){
+    request(app)
+    .get('/todos')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, done);
+  });
 });
-      
+
+describe('GET', function(){
+  it('responds with a single todo item in JSON based on the author', function(done){
+    request(app)
+    .get('/todos/mies')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, done);
+  });
+});
